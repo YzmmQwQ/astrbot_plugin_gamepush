2c351d13e170451bf10cf7d3e95be70fdcc2b5eb
rainbowwarmth <3445487890@qq.com>
2025-09-08 14:47:27 +0800

    refactor: 移除未使用的bot模块，优化群消息发送逻辑

--- model/base.js
+++ model/base.js
@@ -70,21 +70,37 @@ export default class base {
    */
   getScreenData(game, type = "") {
     const currentDate = this.getCurrentDate()
+    let basic
+    if (BotName === "Karin") {
+      basic = {
+        tplFile: `${pluginPath}/resources/html/GamePush-Plugin/GamePush-Plugin.html`,
+        fontsPath: `${pluginPath}/resources/fonts/`,
+        pluResPath: `${pluginPath}/resources/`,
+        htmlSavePath: `${this._path}/@karinjs/${pluginName}/html/`,
+        plugin: {
+          name: "karin-plugin-GamePush",
+          version: PluginPackage.version
+        }
+      }
+    } else {
+      basic = {
+        tplFile: path.join(
+          this._path,
+          "plugins/GamePush-Plugin/resources/html/GamePush-Plugin/GamePush-Plugin.html"
+        ),
+        fontsPath: path.join(this._path, "plugins/GamePush-Plugin/resources/fonts/"),
+        pluResPath: path.join(this._path, "plugins/GamePush-Plugin/resources/"),
+        htmlSavePath: path.join(this._path, "tmp/html/GamePush-Plugin"),
+        plugin: {
+          name: "GamePush-Plugin",
+          version: PluginPackage.version
+        }
+      }
+    }
     const other = {
       saveId: `push_${game}_${type}_${currentDate}`,
       cwd: this._path,
       htmlFileName: `${game}_${type}_${currentDate}.html`,
-      tplFile: `${pluginPath}/resources/html/${pluginName}/GamePush-Plugin.html`,
-      fontsPath: `${pluginPath}/resources/fonts/`,
-      pluResPath: `${pluginPath}/resources/`,
-      htmlSavePath:
-        BotName === "Karin"
-          ? `${this._path}/@karinjs/${pluginName}/html/`
-          : `${this._path}/tmp/html/GamePush-Plugin/`,
-      plugin: {
-        name: pluginName,
-        version: PluginPackage.version
-      },
       bot: {
         name: BotName
       }
@@ -100,6 +116,7 @@ export default class base {
 
     return {
       ...other,
+      ...basic,
       gameName: this.getGameName(game),
       icon: icons[game]
     }
